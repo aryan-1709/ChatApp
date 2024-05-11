99c617653295974bc1683e1af5a6863d975fc286
aryan-1709 <151056485+aryan-1709@users.noreply.github.com>
2024-05-11 10:10:04 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,78 +1,112 @@
 const express = require("express");
 const app = express();
+const http = require("http");
 const mongoose = require("mongoose");
 const Schema = require("./TestSchema");
 const cors = require("cors");
 const MessageSchema = require("./MessageSchema");
 const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// io.on('connection', (socket)=>{
+//   console.log("connected", socket.id)
+//   socket.broadcast.emit("brodcast", socket.id);//message goes to other user than the current one
+//   socket.emit("welcome", "Welcome Again"); //message goes to same user
+//   socket.on("disconnect", ()=>{
+//     console.log("user diconnected", socket.id);
+//   })
+//   socket.on("message",(m)=>{
+//     console.log(m);
+//     socket.broadcast.emit("receive", m);
+//     io.to(m.id).emit("receive", data);
+//   })
+
+// });
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("check");
+});
 
 const url =
   "mongodb+srv://mern_reboot:mern_reboot@userdb.v8snhp1.mongodb.net/?retryWrites=true&w=majority&appName=UserDB";
-app.use(cors());
-app.use(bodyParser.json());
 
-app.listen(5000, () => console.log("Running"));
+server.listen(5000, () => {
+  console.log("server started");
+});
+// app.listen(5000, () => console.log("Running"));
 
 mongoose.connect(url).then((results) => console.log("connected"));
-const ind = 2;
-app.post("/api/data", (req, res) => {
-  const dataReceived = req.body; // Data sent from the client
-  console.log("Data received:", dataReceived.id);
-  // Process the data or send a response back to the client
-  
-  Schema.findOne({name: dataReceived.name}).then((result)=>{
-    const obj = result.message[0];
-  
-    const messageCount = Object.keys(obj).length;
-    console.log("Number of messages:", messageCount);
 
-    console.log("messeges found",obj);
-  })
 
-  Schema.findOneAndUpdate(
-    { name: dataReceived.name }, // Find the document with the given name
-    {
-      $set: { [`message.${ind}.id`]:dataReceived.id
-      },
-      $push: { [`message.${ind}.msgs`]: {  // Push a new message object to the "msgs" array inside the first message object
-        message: dataReceived.message,
-        messageType: dataReceived.messageType,
-        time: dataReceived.time,
-      }}
-    },
-    { new: true, upsert: true } // Options: return the updated document and create if it doesn't exist
-  ).then((result) => {
-    // console.log('Document updated successfully:', result.message[0]);
-    Schema.find().then((result) => {
-      app.get("/get", (req, res) => {
-        res.send(result);
-      });
+io.on("connection", (socket) => {
+  console.log("New Socket Connected", socket.id);
+
+  socket.on("reconnect", (attemptNumber) => {
+    console.log("Reconnected after %d attempts", attemptNumber);
+  });
+
+  Schema.find().then((result) => {
+    socket.emit("get", result);
+    // app.get("/get", (req, res) => {
+    //   res.send(result);
+    // });
+  });
+
+  socket.on("auth", (info) => {
+    const dataReceived = info.user; 
+    console.log(dataReceived.uid)
+    const data = new Schema({
+      _id: dataReceived.uid,
+      name: dataReceived.displayName,
+      status: "student",
     });
-  }).catch((err) => {
-    console.error('Error updating document:', err);
+    console.log(data._id);
+    Schema.findOne({ _id: data._id }).then((existingData) => {
+      if (!existingData) {
+        data.save();
+        console.log("Created User");
+      } else {
+        console.log("already exists");
+      }
+    });
   });
 
-  res.send("Data received successfully");
-});
+  socket.on("msg", (msg)=>{
+    const dataReceived = msg; // Data sent from the client
+    console.log("data received", msg)
+    console.log("Data received:", dataReceived.id);
+    
+    Schema.findOneAndUpdate(
+      { name: dataReceived.name }, // Find the document with the given name
+      {
+        $set: { [`message.0.id`]:dataReceived.id
+        },
+        $push: { [`message.0.msgs`]: { 
+          message: dataReceived.message,
+          messageType: dataReceived.messageType,
+          time: dataReceived.time,
+        }}
+      },
+      { new: true, upsert: true } 
 
-const data = new Schema({
-  name: "Zoro",
-  status: "syudent",
-  curr: "Hay aryan",
-});
-
-Schema.findOne({name: data.name})
-  .then(existingData => {
-    if(!existingData){
-      data.save();
-    }
-    else{
-      console.log("already exist");
-    }
+    ).then((result) => {
+      Schema.find().then((result) => {
+        app.get("/get", (req, res) => {
+          res.send(result);
+        });
+      });
+    }).catch((err) => {
+      console.error('Error updating document:', err);
+    });
   })
-Schema.find().then((result) => {
-  app.get("/get", (req, res) => {
-    res.send(result);
-  });
 
 });
