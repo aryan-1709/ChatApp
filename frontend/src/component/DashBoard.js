91e02958a8a29fe375b2889aff1fdfccdf38f16c
Aryan <av951174188@gmail.com>
2024-06-24 23:27:37 +0530

    Added new files and some changes

---
+++ frontend/src/component/DashBoard.js
@@ -0,0 +1,41 @@
+import * as React from "react";
+import Dropdown from "../pages/Dropdown";
+// import { useRef, useState, useEffect } from "react";
+// import { useContext } from "react";
+// import useSocket from "./Socket";
+
+function DashBoard() {
+  return (
+    <div className="flex gap-5 justify-between py-1 pr-20 pl-6 bg-slate-300 border border-solid border-slate-950 max-md:flex-wrap max-md:px-5">
+      <div className="mt-3">
+        <Dropdown />
+      </div>
+
+      <div className="flex gap-3 whitespace-nowrap">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/6aa4e78a2e90e1f26c4fee0acaea9372910782940594493ce1a6dfd1de9e56d5?apiKey=0387e70fd0ca4fd697a1441ac9e964d1&"
+          alt="Not found"
+          className="shrink-0 aspect-[1.11] w-[67px]"
+        />
+        <div className="flex gap-3.5 my-auto">
+          <div className="flex flex-col flex-1 justify-center my-auto">
+            <div className="self-end text-base font-semibold text-right text-slate-950">
+              Abednego
+            </div>
+            <div className="flex gap-1 text-sm text-center text-zinc-500">
+              <div className="grow">Property Owner</div>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/1154ee703093123e1a47296595a665e031bcb079640e4014b549efe912b4c7d0?apiKey=0387e70fd0ca4fd697a1441ac9e964d1&"
+                alt="Not found"
+                className="shrink-0 my-auto aspect-square w-[15px]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export { DashBoard};
--- frontend/src/component/FrontPage.js
+++ frontend/src/component/FrontPage.js
@@ -1,21 +1,15 @@
 import React from 'react'
-import {DashBoard, responseData }from '../DashBoard'
-import Chats from './Chats'
-import Dropdown from '../Dropdown'
-import Box from '../box'
+import {DashBoard}from './DashBoard'
+import MessagingApp from './MessagingApp'
 export default function FrontPage() {
-  // console.log("responceData", responseData);
   return (
     <div className='overflow-hidden'>
-      {/* <Box width="120vh" height="100vh" backgroundColor="white" className="flex items-center"></Box> */}
-        <div>
-            <DashBoard />
-        </div>
-        <div className='h-[767px] bg-slate-300 '>
-            <Chats />
-        </div>
-        
-        
+      <div>
+        <DashBoard />
+      </div>
+      <div className='h-[767px] bg-slate-300 w-full'>
+        <MessagingApp />
+      </div>
     </div>
   )
 }
